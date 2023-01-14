89048cfb024a9783fa3d780914b1998f705f9bc5
Renij Shrestha <renijshrestha8@gmail.com>
2023-01-14 17:27:00 +0545

    photography

---
+++ src/components/Portfolio/PhotoCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function PhotoCard(props) {
+  return (
+    <>
+      {props.photographyInfo.map((info) => {
+        return (
+          <div className="w-64 p-4 bg-blue-200 rounded-3xl">
+            <img
+              src={`img/photography/${info}.jpg`}
+              alt=""
+              className="h-full rounded-2xl object-cover"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+}
